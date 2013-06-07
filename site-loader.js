fa0cddcd0798e0c67bb3e6e325afd491957ffa57
Ryan Zec <code@ryanzec.com>
2013-06-06 23:05:08 -0400

    Code clean up

--- site-loader.js
+++ site-loader.js
@@ -8,34 +8,34 @@ angular.module('nag.siteLoader', [
     return {
       enableBlocking: function(html) {
         if(!html) {
-          html = '<div class="nag-site-loader-text">Loading Application</div>';
+          html = '<div class="site-loader-text">Loading Application</div>';
         }
 
-        if($('#nag-site-loader').length === 0) {
-          $('body').append('<div id="nag-site-loader" class="nag-site-loader">' + html + '</div>');
+        if($('#site-loader').length === 0) {
+          $('body').append('<div id="site-loader" class="site-loader">' + html + '</div>');
         }
       },
 
       disableBlocking: function() {
-        if($('#nag-site-loader').length !== 0) {
-          $('#nag-site-loader').remove();
+        if($('#site-loader').length !== 0) {
+          $('#site-loader').remove();
         }
       },
 
       isBlockingActive: function() {
-        return $('#nag-site-loader').length !== 0;
+        return $('#site-loader').length !== 0;
       },
 
       enableNonBlocking: function(options) {
         var self = this;
 
         var notifyOptions = _.extend({
-          content: '<div class="nag-message nag-message-notice">loading data</div>',
+          content: '<div class="message notice">loading data</div>',
           autoCloseDelay: false,
           closeOnClick: false,
           verticalPosition: 'top',
           horizontalPosition: 'middle',
-          cssClass: 'nag-non-blocking-loader',
+          cssClass: 'non-blocking-loader',
           margin: 0
         }, options);
 

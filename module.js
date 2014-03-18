c813dbedb9cc81423f29e36c9bdfe471ef98a34d
Ryan Zec <code@ryanzec.com>
2014-03-18 06:17:02 -0400

    coding standards update

--- module.js
+++ module.js
@@ -1,3 +1,10 @@
+/**
+ * # Site Loader
+ *
+ * Site loading indicator that are either blocking or non-block the UI
+ *
+ * @module nag.siteLoader
+ */
 angular.module('nag.siteLoader', [
   'nag.notify'
 ]);
